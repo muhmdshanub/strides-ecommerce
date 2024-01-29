42a004807490fcbbc01bcbcf0d69ed3fc9173775
Muhammad shanoob a k <60563775+muhmdshanub@users.noreply.github.com>
2024-01-29 21:50:29 +0530

    Update addPredefinedAdmin.js

--- addPredefinedAdmin.js
+++ addPredefinedAdmin.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const Admin = require('./models/adminModel');
 
-mongoose.connect('mongodb://localhost:27017/strides');
+mongoose.connect('');
 
 async function createAdminUser() {
     try {
         const existingAdmin = await Admin.findOne({ email: 'admin@gmail.com' });
 
         if (!existingAdmin) {
-            const hashedPassword = await bcrypt.hash('adminadmin', 10);
+            const hashedPassword = await bcrypt.hash('', 10);
 
             const newAdmin = new Admin({
                 name: 'Muhammed Shanoob',
-                phone: '+918157882662',
-                email: 'admin@gmail.com',
+                phone: '',
+                email: '',
                 password: hashedPassword,
                 dateOfBirth: '11-11-1997'
                 // Add other properties as needed
